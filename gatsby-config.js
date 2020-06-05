684b6bdacd431ac078a7c0f09c024e0beb18a59f
Felix Anducho <felix.anducho74@gmail.com>
2020-06-04 19:41:16 -0500

    Add files

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Frontend Masters Gatsby Workshop',
+        descriptions: 'A site built to learn how Gatsby works'
+    },
+    plugins: ['gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet'],
+}
